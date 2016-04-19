d6794d7388756acd9172b2f2e02d9060d2834fbe
anshul0258 <anshul0258@gmail.com>
2016-04-19 17:37:14 +0530

    for issue.

---
+++ POC.Jasmine/Scripts/_references.js
@@ -0,0 +1,7 @@
+﻿/// <reference path="jasmine/jasmine-html.js" />
+/// <reference path="jasmine/jasmine.js" />
+/// <reference path="jasmine/console.js" />
+/// <reference path="jasmine/boot.js" />
+/// <reference path="jquery-2.2.3.js" />
+/// <reference path="bootstrap.js" />
+/// <reference path="spechelpers/spechelper.js" />
